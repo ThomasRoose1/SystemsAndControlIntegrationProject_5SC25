ba7a939c71834030b3e3132390041addb9527aaf
Thomas <thomas-roose@outlook.com>
2026-05-26 10:19:07 +0200

    added observer parameters to init for exp

--- Experiment/BallANDPlate/Init_workspace.m
+++ Experiment/BallANDPlate/Init_workspace.m
@@ -24,3 +24,39 @@ uC = repmat(uC,[1,P]);
 
 path = quintic(-0.0289,3,0,1/fs);
 
+%% ball dynamics
+g = 9.81;                  % Gravity (m/s^2)
+km_factor = (5/7) * g;     % Rolling ball constant (~7.0071)
+
+% Continuous Matrices (4 states, 2 inputs, 2 outputs)
+Ac = [0 1 0 0;
+      0 0 0 0;
+      0 0 0 1;
+      0 0 0 0];
+  
+Bc = [0          0;
+      0          km_factor;
+      0          0;
+     -km_factor  0];
+  
+Cc = [1 0 0 0;
+      0 0 1 0];
+  
+Dc = [0 0;
+      0 0];
+
+%% State observer 
+Ts_fast = 0.001; % 1000 Hz sample time matching the simulation rate
+
+% Discretize continuous matrices for the fast estimation rate
+sys_fast = c2d(ss(Ac, Bc, Cc, Dc), Ts_fast, 'zoh');
+A_fast = sys_fast.A;
+B_fast = sys_fast.B;
+C_fast = sys_fast.C;
+
+% Define your fast observer poles (safely inside the unit circle)
+observer_poles = [0.90, 0.91, 0.92, 0.93];
+
+% Compute the Observer Gain Matrix L
+L = place(A_fast', C_fast', observer_poles)';
+
