4e98fcb47defcbc14b8f48275bf062a3935f11f5
Alexis Santucci <alexis.santuccipro@gmail.com>
2026-06-04 10:55:55 +0200

    Added the float conversion in the send ball function

--- 3D Camera/Phase_F Float Coordinates.py
+++ 3D Camera/Phase_F Float Coordinates.py
@@ -84,22 +84,18 @@ def pixel_to_control_coords(pixel_xy):
 
 
 def send_ball_position_xyz_mm(ctrl_xy, z_value, detected_flag):
-    x_mm = int(ctrl_xy[0])
-    y_mm = int(ctrl_xy[1])
-    z_mm = int(z_value)
 
-    x_bytes = x_mm.to_bytes(2, byteorder='little', signed=True)
-    y_bytes = y_mm.to_bytes(2, byteorder='little', signed=True)
-    z_bytes = z_mm.to_bytes(2, byteorder='little', signed=True)
+    x_mm = float(ctrl_xy[0])
+    y_mm = float(ctrl_xy[1])
+    z_mm = float(z_value)
 
-    packet = bytearray([
-        x_bytes[0], x_bytes[1],
-        0, 0,
-        y_bytes[0], y_bytes[1],
-        0, 0,
-        z_bytes[0], z_bytes[1],
+    packet = struct.pack(
+        '<fffB',
+        x_mm,
+        y_mm,
+        z_mm,
         detected_flag
-    ])
+    )
     if DEBUG_PRINT_UDP:
         print(f"UDP -> X={x_mm}, Y={y_mm}, Z={z_mm}, Flag={detected_flag}")
     UDPClientSocket.sendto(packet, serverAddressPort)
