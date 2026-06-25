25cfe4f297ff6d60a0c6c0ac57269306eab30331
Roose <t.g.roose@student.tue.nl>
2026-06-25 19:02:13 +0200

    exp session, PID recording and MPC improvement

--- Experiment/BallANDPlate/Ball_and_Plate_MicroLabBox_student_rti1202/Ball_and_Plate_MicroLabBox_student_rti.c
+++ Experiment/BallANDPlate/Ball_and_Plate_MicroLabBox_student_rti1202/Ball_and_Plate_MicroLabBox_student_rti.c
@@ -6,7 +6,7 @@
    the hardware and software interrupts used.
 
    RTI1202 7.15 (02-Nov-2020)
-   Thu Jun 25 16:40:41 2026
+   Thu Jun 25 18:53:33 2026
 
    Copyright 2026, dSPACE GmbH. All rights reserved.
 
